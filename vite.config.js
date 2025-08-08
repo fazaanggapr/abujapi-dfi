f3ec88b5b4d00c08c4784ddbfb23dd949ebb6817
fazaanggapr <fpramuditio@gmail.com>
2025-08-08 14:20:33 +0700

    vite config js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Ganti dengan host yang muncul di error kamu (bisa dynamic jika pakai ngrok)
-const allowedHost = '613ba3fe8d39.ngrok-free.app"'
+const allowedHost = '613ba3fe8d39.ngrok-free.app'
 
 export default defineConfig({
   plugins: [react()],

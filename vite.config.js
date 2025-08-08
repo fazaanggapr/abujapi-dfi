af6b92bbcfaa8a497ad235464c4ed3a1587e7242
fazaanggapr <fpramuditio@gmail.com>
2025-08-08 15:36:17 +0700

    Private Route

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Ganti dengan host yang muncul di error kamu (bisa dynamic jika pakai ngrok)
+const allowedHost = '613ba3fe8d39.ngrok-free.app'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,  // Biarkan Vite mendengarkan semua host
-  },
-});
+    host: true,
+    allowedHosts: [allowedHost] 
+  }
+})
